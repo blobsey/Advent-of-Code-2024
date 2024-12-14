5d76e1a32c9723323d253a01d3e9f9eb39a04cfc
blobsey <me@blobsey.com>
2024-12-13 20:10:59 -0800

    refactor day13

--- src/bin/day13.rs
+++ src/bin/day13.rs
@@ -52,24 +52,30 @@ impl ClawMachine {
     }
 
     fn from_str_p2(input: &str) -> Option<Self> {
-        let re = Regex::new(r"(?x)
-            Button\ A:\ X\+(\d+),\ Y\+(\d+)\n   # Parse a1 and a2
-            Button\ B:\ X\+(\d+),\ Y\+(\d+)\n   # Parse b1 and b2
-            Prize:\ X=(\d+),\ Y=(\d+)           # Parse k1 and k2
-        ").ok()?;
+        Self::from_str(input).map(|mut c| {
+            c.k1 += 10_000_000_000_000.0;
+            c.k2 += 10_000_000_000_000.0;
+            c
+        })
+    }
+}
 
-        if let Some(caps) = re.captures(input) {
-            Some(ClawMachine::new(
-                caps[1].parse().ok()?,
-                caps[2].parse().ok()?,
-                caps[3].parse().ok()?,
-                caps[4].parse().ok()?,
-                caps[5].parse::<f64>().ok()? + 10_000_000_000_000.0,
-                caps[6].parse::<f64>().ok()? + 10_000_000_000_000.0,
-            ))
-        } else {
-            None
-        }
+fn cramers_rule(c: ClawMachine) -> Option<(u64, u64)> {
+    let determinant = c.a1 * c.b2 - c.a2 * c.b1;
+
+    if determinant == 0_f64 {
+        return None;
+    }
+
+    let determinant_x = c.k1 * c.b2 - c.k2 * c.b1;
+    let determinant_y = c.a1 * c.k2 - c.a2 * c.k1;
+    let x = determinant_x / determinant;
+    let y = determinant_y / determinant;
+    if x.fract().abs() < 1e-10 && y.fract().abs() < 1e-10 && x > -1e-10 && y > -1e-10 {
+        Some((x as u64, y as u64))
+    }
+    else {
+        None
     }
 }
 
@@ -93,25 +99,6 @@ fn part1(input: &str) {
         .sum();
 
     println!("The answer to part 1 is: {solution}");
-}
-
-fn cramers_rule(c: ClawMachine) -> Option<(u64, u64)> {
-    let determinant = c.a1 * c.b2 - c.a2 * c.b1;
-
-    if determinant == 0_f64 {
-        return None;
-    }
-
-    let determinant_x = c.k1 * c.b2 - c.k2 * c.b1;
-    let determinant_y = c.a1 * c.k2 - c.a2 * c.k1;
-    let x = determinant_x / determinant;
-    let y = determinant_y / determinant;
-    if x.fract().abs() < 1e-10 && y.fract().abs() < 1e-10 && x > -1e-10 && y > -1e-10 {
-        Some((x as u64, y as u64))
-    }
-    else {
-        None
-    }
 }
 
 fn part2(input: &str) {
